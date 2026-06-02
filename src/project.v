c6a01d41120f0342ec6bc2796e505f355b850158
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 12:02:56 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,33 +1,33 @@
 module tt_um_encoder (
-    input  wire [7:0] ui_in,     // 8 input pins
-    output wire [7:0] uo_out,    // 8 output pins
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
 
-    reg [2:0] encoded;
+reg [2:0] code;
 
-    always @(*) begin
-        case (ui_in)
-            8'b00000001: encoded = 3'b000;
-            8'b00000010: encoded = 3'b001;
-            8'b00000100: encoded = 3'b010;
-            8'b00001000: encoded = 3'b011;
-            8'b00010000: encoded = 3'b100;
-            8'b00100000: encoded = 3'b101;
-            8'b01000000: encoded = 3'b110;
-            8'b10000000: encoded = 3'b111;
-            default:     encoded = 3'b000;
-        endcase
-    end
+always @(*) begin
+    casez(ui_in)
+        8'b???????1: code = 3'd0;
+        8'b??????10: code = 3'd1;
+        8'b?????100: code = 3'd2;
+        8'b????1000: code = 3'd3;
+        8'b???10000: code = 3'd4;
+        8'b??100000: code = 3'd5;
+        8'b?1000000: code = 3'd6;
+        8'b10000000: code = 3'd7;
+        default:     code = 3'd0;
+    endcase
+end
 
-    assign uo_out = {5'b00000, encoded};
+assign uo_out = {5'b0, code};
 
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
 
 endmodule
