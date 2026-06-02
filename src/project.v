8d5c7ad1ee6a7086239d52b6ced2af5e0f4ed4f2
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 11:40:34 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,33 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_encoder (
+    input  wire [7:0] ui_in,     // 8 input pins
+    output wire [7:0] uo_out,    // 8 output pins
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg [2:0] encoded;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    always @(*) begin
+        case (ui_in)
+            8'b00000001: encoded = 3'b000;
+            8'b00000010: encoded = 3'b001;
+            8'b00000100: encoded = 3'b010;
+            8'b00001000: encoded = 3'b011;
+            8'b00010000: encoded = 3'b100;
+            8'b00100000: encoded = 3'b101;
+            8'b01000000: encoded = 3'b110;
+            8'b10000000: encoded = 3'b111;
+            default:     encoded = 3'b000;
+        endcase
+    end
+
+    assign uo_out = {5'b00000, encoded};
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
