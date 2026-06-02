e8d459f1447ec34e3c8e338798007157f9ff3d65
kavinnathan5077-art <kavinnathan5077@gmail.com>
2026-06-02 12:09:31 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,19 +12,18 @@ module tt_um_encoder (
 reg [2:0] code;
 
 always @(*) begin
-    casez(ui_in)
-        8'b???????1: code = 3'd0;
-        8'b??????10: code = 3'd1;
-        8'b?????100: code = 3'd2;
-        8'b????1000: code = 3'd3;
-        8'b???10000: code = 3'd4;
-        8'b??100000: code = 3'd5;
-        8'b?1000000: code = 3'd6;
+    case (ui_in)
+        8'b00000001: code = 3'd0;
+        8'b00000010: code = 3'd1;
+        8'b00000100: code = 3'd2;
+        8'b00001000: code = 3'd3;
+        8'b00010000: code = 3'd4;
+        8'b00100000: code = 3'd5;
+        8'b01000000: code = 3'd6;
         8'b10000000: code = 3'd7;
         default:     code = 3'd0;
     endcase
 end
-
 assign uo_out = {5'b0, code};
 
 assign uio_out = 8'b0;
